b24e582dd666a35b57d90392e5b5c97607c65217
vyshnavi-koleti <vyshnavikoleti.vk@gmail.com>
2021-09-26 13:28:03 +0530

    refactor: comments removed

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
-// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
@@ -18,9 +17,7 @@ function errorHandler(error) {
 
 function clickHandler() {
 
-    var inputText = textInput.value; //taking input
-
-    //calling server
+    var inputText = textInput.value; 
 
     fetch(getTranslationURL(inputText))
         .then(response => response.json())
